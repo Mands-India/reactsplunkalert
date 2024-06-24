6212d01ab30c1c72b30073aa0d59e6d1ec6a8a84
Abhishek Kumar <abhishek.kumar@mandsconsulting.com>
2024-06-24 10:10:06 +0530

    added

--- index.js
+++ index.js
@@ -1,62 +1,62 @@
 
-let https;
-if (typeof window === 'undefined') {
-  // In Node.js environment
-  https = require('https');
-} else {
-  // In browser environment
-  https = require('https-browserify');
-}
+// let https;
+// if (typeof window === 'undefined') {
+//   // In Node.js environment
+//   https = require('https');
+// } else {
+//   // In browser environment
+//   https = require('https-browserify');
+// }
 async function reactSplunkLog(splunkHost, splunkToken, splunkIndex, errorMessage){
-    
-        const eventData = {
-            event: {
-                "message": errorMessage,
-                "index": splunkIndex 
-            }
-        };
+    return 'hello';
+        // const eventData = {
+        //     event: {
+        //         "message": errorMessage,
+        //         "index": splunkIndex 
+        //     }
+        // };
         
-        const options = {
-            hostname: splunkHost,
-            port: '8088',
-            path: '/services/collector',
-            method: 'POST',
-            headers: {
-                'Authorization': `Splunk ${splunkToken}`,
-                'Content-Type': 'application/json',
-            },
-            agent: new https.Agent({
-                rejectUnauthorized: false
-            })
-        };
+        // const options = {
+        //     hostname: splunkHost,
+        //     port: '8088',
+        //     path: '/services/collector',
+        //     method: 'POST',
+        //     headers: {
+        //         'Authorization': `Splunk ${splunkToken}`,
+        //         'Content-Type': 'application/json',
+        //     },
+        //     agent: new https.Agent({
+        //         rejectUnauthorized: false
+        //     })
+        // };
     
-        try {
-            const responseBody = await new Promise((resolve, reject) => {
-                const req = https.request(options, (res) => {
-                    let responseBody = '';
-                    res.on('data', (d) => {
-                        responseBody += d.toString();
-                    });
-                    res.on('end', () => {
-                        const responseObj = {
-                            text: JSON.parse(responseBody).text,
-                            statusCode: res.statusCode
-                        };
-                        resolve(responseObj);
-                    });
-                });
-                req.on('error', (error) => {
-                    reject(error);
-                });
-                req.write(JSON.stringify(eventData));
-                req.end();
-            });
+        // try {
+        //     const responseBody = await new Promise((resolve, reject) => {
+        //         const req = https.request(options, (res) => {
+        //             let responseBody = '';
+        //             res.on('data', (d) => {
+        //                 responseBody += d.toString();
+        //             });
+        //             res.on('end', () => {
+        //                 const responseObj = {
+        //                     text: JSON.parse(responseBody).text,
+        //                     statusCode: res.statusCode
+        //                 };
+        //                 resolve(responseObj);
+        //             });
+        //         });
+        //         req.on('error', (error) => {
+        //             reject(error);
+        //         });
+        //         req.write(JSON.stringify(eventData));
+        //         req.end();
+        //     });
             
-            return responseBody;
-        } catch (error) {
-            console.error('Final Error:', error);
-            return error;
-        }
+        //     return responseBody;
+        // } catch (error) {
+        //     console.error('Final Error:', error);
+        //     return error;
+        // }
     }
     
     module.exports = reactSplunkLog
